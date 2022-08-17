248f89b7c11b37b1fc421630373919ecdcd11cf6
mattcooperdev <coopandmgc@gmail.com>
2022-08-17 13:15:59 +0000

    [Admin] Customised Admin for product and category creation

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Categories'   
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+        verbose_name_plural = 'Categories'
+        name = models.CharField(max_length=254)
+        friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
