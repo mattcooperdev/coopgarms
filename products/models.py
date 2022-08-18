7de115e4f87e850dc3f50fe84f413bb31bd14dd7
mattcooperdev <coopandmgc@gmail.com>
2022-08-18 12:17:11 +0000

    [Build] Added search functionality and customised product form

--- products/models.py
+++ products/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        name = models.CharField(max_length=254)
-        friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
