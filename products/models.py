b7be858f33e2d48e7ab0b49f40dc0f7ab5f2eb54
mattcooperdev <coopandmgc@gmail.com>
2022-08-21 17:40:11 +0000

    [Clean] Tidy up style errors in products app

--- products/models.py
+++ products/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-
+    '''Model to identify category of product'''
     class Meta:
+        '''inner class to modify naming'''
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -12,17 +13,21 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
+        '''to return friendly name from Class'''
         return self.friendly_name
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    '''Model for product characteristics'''
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
