4d98f73f7230d08a2d555af4e6bd0c6222abd9ce
mattcooperdev <mattcooperdev@gmail.com>
2022-08-27 17:55:37 +0000

    [Fix] Add brand field to Product model and updated Main Nav

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    brand = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
