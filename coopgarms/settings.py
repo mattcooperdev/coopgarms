d43f1c8c50a47e3bcb15fef9c87e2b9c5d1319fa
mattcooperdev <mattcooperdev@gmail.com>
2022-09-03 17:55:54 +0000

    Updated checkout url and created checkout js file for stripe elements

--- coopgarms/settings.py
+++ coopgarms/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
