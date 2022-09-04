41eea6c81ae17acf1a83e969cf45edd1a470bfe0
mattcooperdev <mattcooperdev@gmail.com>
2022-09-04 17:57:52 +0000

    [Bug] Added missing bracket in product-info to render cart correctly

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     ''' Update subtotal in bag '''
--- bag/views.py
+++ bag/views.py
@@ -53,6 +53,7 @@ def add_to_bag(request, item_id):
             messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
 
 
