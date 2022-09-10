771a1ea540c19128f16f39384116ad6d06c4d0db
mattcooperdev <mattcooperdev@gmail.com>
2022-09-10 19:03:10 +0000

    Updated debug for production

--- coopgarms/settings.py
+++ coopgarms/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xxd318-=yh%6mq#)9@olv$v+h6e2+j)pz_b-s6-w-q@@4n-f@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['coopgarms.herokuapp.com', 'localhost']
 
