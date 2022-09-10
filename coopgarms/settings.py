81bb3022abd4a9378c744b713182f6b9fac39d60
mattcooperdev <mattcooperdev@gmail.com>
2022-09-10 19:08:51 +0000

    Updated secret key in settings

--- coopgarms/settings.py
+++ coopgarms/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xxd318-=yh%6mq#)9@olv$v+h6e2+j)pz_b-s6-w-q@@4n-f@%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
