acac2502156fa83c41c48cdc90f3d2a713e9fac5
mattcooperdev <mattcooperdev@gmail.com>
2022-09-12 18:36:25 +0000

    Removed secret key and set debug

--- coopgarms/settings.py
+++ coopgarms/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%37n(ad(c)*l8p-2+u_wd4_=p29&wovvnk+t*m9xp7q__k_9e^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['coopgarms.herokuapp.com', 'localhost']
 
