6a9368e451223a2ebab01f134e77cb310f606909
mattcooperdev <mattcooperdev@gmail.com>
2022-09-13 09:00:57 +0000

    Made changes to stripe info in settings

--- coopgarms/settings.py
+++ coopgarms/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['coopgarms.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['coopgarms.herokuapp.com',
+                 '127.0.0.1:8000',
+                 '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-mattcooperdev-coopgarms-9vbe9xekykq.ws-eu64.gitpod.io']
 
@@ -206,9 +208,9 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
-FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'eur'
+FREE_DELIVERY_THRESHOLD = 0
+STANDARD_DELIVERY_PERCENTAGE = 5
+STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
